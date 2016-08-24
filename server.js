148d7ea238ad76a29bd868ea157222cd769f52cb
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-08-24 18:40:10 -0400

    bitch

--- server.js
+++ server.js
@@ -48,30 +48,30 @@ app.listen(PORT, function() {
 })
 
  
-app.use(express.static(__dirname + '/client'));
+// app.use(express.static(__dirname + '/client'));
  
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
  
-// Sendmail route
-app.post('/sendmail', function(req, res){
-    var options = {
-        auth: {
-            api_key: 'YOUR_SENDGRID_API_KEY'
-        }
-    }
-    var mailer = nodemailer.createTransport(sgTransport(options));
-    mailer.sendMail(req.body, function(error, info){
-        if(error){
-            res.status('401').json({err: info});
-        }else{
-            res.status('200').json({success: true});
-        }
-    });
-});
+// // Sendmail route
+// app.post('/sendmail', function(req, res){
+//     var options = {
+//         auth: {
+//             api_key: 'YOUR_SENDGRID_API_KEY'
+//         }
+//     }
+//     var mailer = nodemailer.createTransport(sgTransport(options));
+//     mailer.sendMail(req.body, function(error, info){
+//         if(error){
+//             res.status('401').json({err: info});
+//         }else{
+//             res.status('200').json({success: true});
+//         }
+//     });
+// });
  
 // Start server
-var port = 8080, ip = "127.0.0.1";
-app.listen(port, ip, function() {
-  console.log('Express server listening on %d', port);
-});
+
+// app.listen(port, ip, function() {
+//   console.log('Express server listening on %d', port);
+// });
